a48151be58c9ada8c2313dada92f34b7fe1cf1d6
cwxia0s <cwxia0s@com>
2024-04-01 16:26:04 +0800

    fix: requirements Versions

--- inference.sh
+++ inference.sh
@@ -4,4 +4,4 @@
 
 # python inference.py --output ../predictions --batch 24 --model ../models/finetune_2.pth --cfg configs/m.yml --data /root/autodl-tmp/Dataset701_AbdomenCT/imagesVal/FLARETs_0014_0000.nii.gz --label /root/autodl-tmp/Dataset701_AbdomenCT/labelsVal/FLARETs_0014.nii.gz
 
-python inference.py --output ./prediction --batch 24 --model ../models/class14_m_701.pth --cfg configs/m.yml --data /root/autodl-tmp/Dataset701_AbdomenCT/imagesVal/FLARETs_0014_0000.nii.gz
+python inference.py --output ../prediction --batch 24 --model ../models/class14_m_701.pth --cfg configs/m.yml --data /root/autodl-tmp/Dataset701_AbdomenCT/imagesVal/FLARETs_0014_0000.nii.gz
